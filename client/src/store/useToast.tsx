b454b6ef5b8c9b52317553950c167d77e6889ef1
RoshanRv <nkroshankumar@gmail.com>
2024-02-11 19:51:52 +0530

    Added Toast

---
+++ client/src/store/useToast.tsx
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+
+interface ToastProp {
+  msg: string;
+  variant: "error" | "success";
+}
+
+const TOAST_DURATION = 5000;
+
+interface UseToastProps {
+  toast: ToastProp;
+  setToast: (toast: ToastProp) => void;
+  showToast: boolean;
+}
+
+const useToast = create<UseToastProps>()((set) => ({
+  toast: {
+    msg: "",
+    variant: "success",
+  },
+  showToast: false,
+  setToast: ({ msg, variant }) =>
+    set((state) => {
+      let toastTimeout: any;
+      if (state.showToast) {
+        clearTimeout(toastTimeout);
+        toastTimeout = setTimeout(() => {
+          set({ showToast: false });
+          return clearTimeout(toastTimeout);
+        }, TOAST_DURATION);
+        return { toast: { msg, variant }, showToast: true };
+      } else {
+        toastTimeout = setTimeout(() => {
+          set({ showToast: false });
+          return clearTimeout(toastTimeout);
+        }, TOAST_DURATION);
+        return { toast: { msg, variant }, showToast: true };
+      }
+    }),
+}));
+
+export default useToast;
