19d1b9a3b606d8989b17aadeecc9a070b5f268aa
Jeyadheesh <jeyadheesh.nagarajan@gmail.com>
2024-02-12 15:00:51 +0530

    summa

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 
 const AdminPage = () => {
   return (
-    <section className="mx-auto grid min-h-[calc(100vh-6rem)] w-max grid-cols-2 items-center justify-center gap-10 ">
-      <Card icon="📄" title="Report Generation" link="/admin/reportgenerate" />
-      <Card icon="🔐" title="Generate Login" link="/admin/generate" />
-      <Card icon="🗃️" title="Update Data" link="" />
-      <Card icon="🗑️" title="Delete Data" link="" />
+    <section className="mx-auto flex min-h-[calc(100vh-6rem)] items-center justify-center gap-0 border border-red-500 ">
+      <div className="grid w-5/12 grid-cols-2 items-center justify-center gap-10">
+        <Card
+          icon="📄"
+          title="Report Generation"
+          link="/admin/reportgenerate"
+        />
+        <Card icon="🔐" title="Generate Login" link="/admin/generate" />
+        <Card icon="🗃️" title="Update Data" link="" />
+        <Card icon="🗑️" title="Delete Data" link="" />
+      </div>
     </section>
   );
 };
@@ -25,7 +31,7 @@ const Card = ({
   <Link to={link}>
     <div className="flex flex-col items-center justify-center gap-3 rounded-lg bg-black p-6 text-white transition-all hover:bg-black/70">
       <p className="text-4xl">{icon}</p>
-      <h2 className="text-xl font-semibold">{title}</h2>
+      <h2 className="text-nowrap text-xl font-semibold">{title}</h2>
     </div>
   </Link>
 );
