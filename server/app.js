159a72a560916aa85c8a3d6758276b2335729133
Jeyadheesh <jeyadheesh.nagarajan@gmail.com>
2024-02-26 14:14:18 +0530

    fix limit error

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 // app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(
   cors({
     origin: ["http://localhost:5173"],
@@ -862,7 +862,7 @@ const transactionProcess = (tablename, insertQuery, insertData, res) => {
         }
 
         console.log("Table Deleted");
-        console.log(insertQuery, insertData);
+        // console.log(insertQuery, insertData);
         // Insert new data
         db.query(insertQuery, [insertData], (error, results) => {
           if (error) {
