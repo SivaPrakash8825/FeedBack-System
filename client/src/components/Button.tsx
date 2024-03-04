1f397f38c6af65c66f8d1c19007aa2b22c2cfe20
RoshanRv <nkroshankumar@gmail.com>
2024-03-04 11:49:06 +0530

    UI fix

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -20,12 +20,11 @@ const Button = ({
   return (
     <button
       onClick={onClick}
-      className={`flex items-center justify-center gap-4 ${disable ? "pointer-events-none  opacity-[.4]" : "cursor-pointer"}  rounded-md border-2 border-black py-2 font-semibold transition-all
-      ${type === "primary" ? "bg-black text-white hover:bg-black/70 focus:bg-black/70" : "bg-white text-black hover:bg-gray-200 focus:bg-gray-200"} ${width === "full" ? "w-full" : "mx-auto w-max px-6"}  `}
+      className={`flex items-center justify-center gap-4 ${disable ? "pointer-events-none  opacity-[.4]" : "cursor-pointer"}  border-primary rounded-md border-2 py-2 font-semibold transition-all
+      ${type === "primary" ? "bg-primary hover:bg-primary/80 focus:bg-primary/80 text-white" : "bg-white text-black hover:bg-gray-200 focus:bg-gray-200"} ${width === "full" ? "w-full" : "mx-auto w-max px-6"}  `}
     >
-      <p className="select-none">{title}</p>
+      <p className="select-none font-semibold">{title}</p>
       {loading && <Spinner size="sm" type={type} />}
-      
     </button>
   );
 };
